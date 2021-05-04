3202874b62dc232affdc1472d96696fc53e66a5c
Souvik Hazra <souvik.babanhazra@gmail.com>
2021-05-04 21:40:47 +0530

    Unzip file implemented

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -1,5 +1,6 @@
 import Foundation
 import Capacitor
+import Zip
 
 /**
  * Please read the Capacitor iOS Plugin Development Guide
@@ -8,10 +9,23 @@ import Capacitor
 @objc(Unzip)
 public class Unzip: CAPPlugin {
 
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-        call.success([
-            "value": value
-        ])
+    @objc func unzipFile(_ call: CAPPluginCall) {
+        let path = URL(string: call.getString("path") ?? "")
+        let password = call.getString("password")
+        let destination = URL(string: call.getString("destination") ?? "")
+        
+        if (path != nil && destination != nil) {
+            do {
+                let tempFolder = "temp"
+                try Zip.unzipFile(path!, destination: destination!.appendingPathComponent(tempFolder), overwrite: true, password: password)
+                call.success([
+                    "output": tempFolder
+                ])
+            } catch {
+                call.reject("Failed to extract zip", "EXTRACTION_FAILED")
+            }
+        } else {
+            call.reject("Failed to find files", "FILE_NOT_FOUND")
+        }
     }
 }
